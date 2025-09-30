32a1782c7fa48a74258633537b7fb82d94bb4390
andynesse <andynesse04@gmail.com>
2025-09-30 22:03:14 +0200

    feat: Add inspect cmd for displaying pokemon info

---
+++ internal/commands/inspect.go
@@ -0,0 +1,28 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/andynesse/go-pokedex/internal/config"
+)
+
+func commandInspect(config *config.Config) error {
+	if len(config.Args) == 0 {
+		return fmt.Errorf("no pokemon to inspect")
+	}
+	pokemon, exists := config.Pokedex.Pokemon[config.Args[0]]
+	if !exists {
+		return fmt.Errorf("pokemon does not exist in pokedex")
+	}
+
+	output := fmt.Sprintf("Name: %s\nHeight: %d\nWeight: %d\nStats:\n", pokemon.Name, pokemon.Height, pokemon.Weight)
+	for k, v := range pokemon.Stats {
+		output += fmt.Sprintf("  -%s: %d\n", k, v)
+	}
+	output += "Types:\n"
+	for _, t := range pokemon.Types {
+		output += fmt.Sprintf("  - %s\n", t)
+	}
+	fmt.Print(output)
+	return nil
+}
